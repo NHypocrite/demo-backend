092d6604131f674381931b0684d88499571a1872
Hpc's Windows <xjtuhpc@163.com>
2022-07-05 10:54:02 +0800

    Added the function for user login.

--- src/main/java/org/xdq/demo/DemoApp.java
+++ src/main/java/org/xdq/demo/DemoApp.java
@@ -15,27 +15,20 @@ public class DemoApp {
 
 
 
-
-    @Bean   // 表示给SpringBoot应用添加一个对象（即将方法的返回值做为一个受spring管理的对象)
-    public WebMvcConfigurer  webMvcConfigurer(){
+    @Bean //表示给SpringBoot应用添加一个对象（即将方法的返回值做为一个受spring管理的对象）
+    public WebMvcConfigurer webMvcConfigurer(){
         return new WebMvcConfigurer() {
-            // 在服务端进行跨域设置
+
+            //在服务端进行跨域设置
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-
                 registry
                         .addMapping("/**")//允许任何样式的地址请求跨域
                         .allowedOriginPatterns("*")//允许任何主机来源跨域
                         .allowedHeaders("*")//允许任何请求头跨域
-                        .allowedMethods("*");//允许任何请求方式跨域
-
-
-
+                        .allowedMethods("*");//允许任何请求方式(get、post 、。。。。。)跨域
             }
         };
-
-
     }
-
 
 }
